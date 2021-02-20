618eb4b8f5c35890eb1cb298c6b5c9c1eb28fddc
VasylChorney <vchorney@easytech.co>
2021-02-20 23:09:14 +0200

    loks

--- src/Counter.java
+++ src/Counter.java
@@ -3,25 +3,21 @@ import static java.lang.Thread.sleep;
 class Counter {
 
   private int c = 0;
-  private Object lock1 = new Object();
-  private Object lock2 = new Object();
 
   public void increment() throws InterruptedException {
-    synchronized (lock1){
-      int a;
-      a = c;
-      a++;
-      c = a;
-    }
+    int a;
+    sleep(150);
+    a = c;
+    a++;
+    c = a;
   }
 
   public void decrement() throws InterruptedException {
-    synchronized (lock2){
-      int a;
-      a = c;
-      a--;
-      c = a;
-    }
+    int a;
+    sleep(100);
+    a = c;
+    a--;
+    c = a;
   }
 
   public int value() {
